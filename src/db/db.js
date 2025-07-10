956dd62b43e30c3c1fa79a2f79fbe9ddc98172e4
Bryan Mwangi <62949543+BryanMwangi@users.noreply.github.com>
2025-07-10 07:54:54 +0200

    minor change

--- src/db/db.js
+++ src/db/db.js
@@ -3,8 +3,8 @@ import { drizzle } from "drizzle-orm/mysql2";
 
 const poolConnection = mysql.createPool({
   host: "localhost",
-  user: "app_user",
-  password: "pass123",
+  user: "my-user",
+  password: "my-password",
   database: "US_DUMMY_DATA",
   waitForConnections: true,
   connectionLimit: 10,
